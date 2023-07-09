480085c80b7827c2c05bfb75cb30adff23811b53
nniederw <niels.niederwieser@gmx.ch>
2023-07-09 19:14:09 +0200

    u

--- GMTK2023/Assets/Scripts/Chest.cs
+++ GMTK2023/Assets/Scripts/Chest.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
 public class Chest : MonoBehaviour, IInteractable
 {
     public List<Item> Content = new List<Item>();
     public bool Looted = false;
     public bool Active = true;
+    [SerializeField] private Sprite OpenedChest;
     public IDamagable Damagable()
     => throw new System.Exception("A chest isn't Damagable");
     public List<Item> Interact()
@@ -14,6 +16,7 @@ public class Chest : MonoBehaviour, IInteractable
         if (!Looted)
         {
             Looted = true;
+            GetComponent<SpriteRenderer>().sprite = OpenedChest;
             return Content;
         }
         return new List<Item>();
@@ -26,7 +29,6 @@ public class Chest : MonoBehaviour, IInteractable
     }
     private void Start()
     {
-        Debug.Log("Chest start");
     }
     private void OnValidate()
     {
